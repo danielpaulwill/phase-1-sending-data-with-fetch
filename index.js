bd5227bb3b8aeee938e4dde9ef78e29bbd79e8c9
Daniel Williams <danielpaulwill@gmail.com>
2021-08-20 08:45:01 -0400

    commit tree 5b2fca092fc2d0952787ed408a1d27c47e3cbab2

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const userData = {
+        name: `${userName}`,
+        email: `${userEmail}`
+    };
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+    })
+        .then(res => res.json())
+        .then(object => {
+           document.body.innerHTML = object.id}
+
+        )
+        .catch(error => {
+            document.body.innerHTML = error.message;
+        })
+
+    };
+
+
+
